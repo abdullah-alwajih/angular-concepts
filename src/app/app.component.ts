79923636bb43c29672a6a58e9a63691afc8d79d4
abdullah-alwajih <pro.abdullah.alwajih@gmail.com>
2024-06-23 08:47:24 +0300

    Built-in pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,54 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {HeaderComponent} from "./shared/components/layout/header/header.component";
 import {ActorFormComponent} from "./features/actor/components/actor-form/actor-form.component";
 import {ProfileEditorComponent} from "./features/profile/components/profile-editor/profile-editor.component";
+import {CurrencyPipe, DatePipe, DecimalPipe, NgClass, UpperCasePipe} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, RouterLink, ActorFormComponent, ProfileEditorComponent],
+  imports: [RouterOutlet, HeaderComponent, RouterLink, ActorFormComponent, ProfileEditorComponent, NgClass, UpperCasePipe, DatePipe, CurrencyPipe, DecimalPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit, OnDestroy {
-  title = 'angular-concepts';
+export class AppComponent {
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production Server',
+      status: 'stable',
+      amount: 522451,
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      amount: 522451,
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      amount: 522451,
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      amount: 522451,
+      started: new Date(15, 1, 2017)
+    }
+  ];
 
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-  }
-
-  ngOnInit() {
-  }
-
-  ngOnDestroy(): void {
+  getStatusClasses(server: { instanceType: string, name: string, status: string, started: Date }) {
+    return {
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical'
+    };
   }
 }
