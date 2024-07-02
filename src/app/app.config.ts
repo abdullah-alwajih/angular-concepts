4cb97c152658b4cf887633e16a088642d8a48212
Abdullah Al-Wajih <Pro.Abdullah.AlWajih@gmail.com>
2024-07-02 12:36:51 +0300

    Response Interceptors & Multiple Interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,12 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {authInterceptor} from "./core/interceptors/auth-interceptor";
+import {loggingInterceptor} from "./core/interceptors/logging-interceptor";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(
-      withInterceptors([authInterceptor])
+      withInterceptors([authInterceptor, loggingInterceptor])
     ),
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes)
